1942ad7c4a8b8441c7c324feef6ceff695fc94ec
まかろに <96648305+saitocastel1900@users.noreply.github.com>
2024-11-08 16:53:41 +0900

    ステージ番号表示UIの改修

--- Assets/Scripts/Widget/InGame/StageNumber/StageNumberView.cs
+++ Assets/Scripts/Widget/InGame/StageNumber/StageNumberView.cs
@@ -1,19 +1,22 @@
+using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace Widget.InGame.StageNumber
 {
+    /// <summary>
+    /// ステージ番号の見た目を管理する
+    /// </summary>
     public class StageNumberView : MonoBehaviour
     {
         /// <summary>
-        /// Text
+        /// TextMeshProUGUI
         /// </summary>
-        [SerializeField] private Text _text;
+        [SerializeField] private TextMeshProUGUI _text;
 
         /// <summary>
-        /// 番号を設定
+        /// ステージ番号を設定
         /// </summary>
-        /// <param name="number">番号</param>
+        /// <param name="number">設定したい番号</param>
         public void SetText(int number)
         {
             _text.text = number.ToString();
